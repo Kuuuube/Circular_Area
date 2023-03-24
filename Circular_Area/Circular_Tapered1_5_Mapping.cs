bd05f88483568a175d0d1fab043fa7e03eb1bc2a
kuuube <hexagonisalie@gmail.com>
2023-03-24 15:30:01 -0400

    Feat: Add Tapered1.5 and Tapered1.5 Inverse mappings

---
+++ Circular_Area/Circular_Tapered1_5_Mapping.cs
@@ -0,0 +1,75 @@
+﻿using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Output;
+using System;
+using System.Numerics;
+
+namespace Circular_Area
+{
+    [PluginName("Circular Tapered1.5 Mapping")]
+    public class Circular_Tapered1_5_Mapping : CircularBase
+    {
+        public static string Filter_Name = "Circular Tapered1.5 Mapping";
+
+        public static Vector2 CircleToSquare(Vector2 input)
+        {
+            double u = input.X;
+            double v = input.Y;            
+            
+            double u2 = Math.Pow(u, 2);
+            double v2 = Math.Pow(v, 2);
+
+            double u4 = Math.Pow(u, 4);
+            double v4 = Math.Pow(v, 4);
+
+            double absu = Math.Abs(u);
+            double absv = Math.Abs(v);
+
+            double sgnu = absu / u;
+            double sgnv = absv / v;
+
+            if (Math.Abs(v) < 0.00001 || Math.Abs(u) < 0.00001)
+            {
+                var circle = new Vector2(
+                        (float)(u),
+                        (float)(v)
+                        );
+
+                return No_NaN(circle, input);
+            }
+            else
+            {
+                var circle = new Vector2(
+                    (float)(sgnu * Math.Sqrt(((1 + (u2 / v2)) / (2 * (3 - 2 * Math.Sqrt(u2 + v2)))) - Math.Sqrt(Math.Pow((1 + (u2 / v2)) / (2 * (3 - 2 * Math.Sqrt(u2 + v2))), 2) - (u2 / v2) * (Math.Pow(Math.Sqrt(u2 + v2), 2) / (3 - 2 * Math.Sqrt(u2 + v2)))))),
+                    (float)(sgnv * Math.Sqrt(((1 + (1 / (u2 / v2))) / (2 * (3 - 2 * Math.Sqrt(u2 + v2)))) - Math.Sqrt(Math.Pow((1 + (1 / (u2 / v2))) / (2 * (3 - 2 * Math.Sqrt(u2 + v2))), 2) - (1 / (u2 / v2)) * (Math.Pow(Math.Sqrt(u2 + v2), 2) / (3 - 2 * Math.Sqrt(u2 + v2))))))
+                    );
+
+                return No_NaN(circle, input);
+            }
+        }
+
+        public override event Action<IDeviceReport> Emit;
+
+        public override void Consume(IDeviceReport value)
+        {
+            if (value is ITabletReport report)
+            {
+                report.Position = Filter(report.Position);
+                value = report;
+            }
+
+            Emit?.Invoke(value);
+        }
+
+        public Vector2 Filter(Vector2 input)
+        {
+            if (CheckQuadrant(ToUnit(input), Filter_Name))
+            {
+                return input;
+            }
+            return FromUnit(Clamp(DiscardTruncation(CircleToSquare(ApplyTruncation(ToUnit(input), Filter_Name)), Filter_Name)));
+        }
+
+        public override PipelinePosition Position => PipelinePosition.PostTransform;
+    }
+}
---
+++ Circular_Area/Circular_Tapered1_5_Mapping_Inverse.cs
@@ -0,0 +1,74 @@
+﻿using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Output;
+using System;
+using System.Numerics;
+
+namespace Circular_Area
+{
+    [PluginName("Circular Tapered1.5 Mapping Inverse")]
+    public class Circular_Tapered1_5_Mapping_Inverse : CircularBase
+    {
+        public static string Filter_Name = "Circular Tapered1.5 Mapping Inverse";
+
+        public static Vector2 SquareToCircle(Vector2 input)
+        {
+            double x = input.X;
+            double y = input.Y;
+
+            double x2 = Math.Pow(x, 2);
+            double y2 = Math.Pow(y, 2);
+
+            if (Math.Abs(y) < 0.00001 || Math.Abs(x) < 0.00001)
+            {
+                var circle = new Vector2(
+                        (float)(x),
+                        (float)(y)
+                        );
+
+                return No_NaN(circle, input);
+            }
+            else
+            {
+                var circle = new Vector2(
+                    (float)((x / Math.Sqrt(x2 + y2)) * ((x2 * y2) + Math.Sqrt(Math.Pow((x2 * y2), 2) - 3 * (x2 * y2) + Math.Pow(Math.Sqrt(x2 + y2), 2)))),
+                    (float)((y / Math.Sqrt(x2 + y2)) * ((x2 * y2) + Math.Sqrt(Math.Pow((x2 * y2), 2) - 3 * (x2 * y2) + Math.Pow(Math.Sqrt(x2 + y2), 2))))
+                    );
+
+                return No_NaN(circle, input);
+            }
+        }
+
+        public override event Action<IDeviceReport> Emit;
+
+        public override void Consume(IDeviceReport value)
+        {
+            if (value is ITabletReport report)
+            {
+                report.Position = Filter(report.Position);
+                value = report;
+            }
+
+            Emit?.Invoke(value);
+        }
+
+        public Vector2 Filter(Vector2 input)
+        {
+            if (CheckQuadrant(ToUnit(input), Filter_Name))
+            {
+                if (GetDisableExpand(false, true, Filter_Name))
+                {
+                    return input;
+                }
+                return FromUnit(Clamp(Expand(ToUnit(input))));
+            }
+            if (GetDisableExpand(true, false, Filter_Name))
+            {
+                return FromUnit(Clamp(DiscardTruncation(SquareToCircle(ApplyTruncation(ToUnit(input), Filter_Name)), Filter_Name)));
+            }
+            return FromUnit(Clamp(Expand(DiscardTruncation(SquareToCircle(ApplyTruncation(ToUnit(input), Filter_Name)), Filter_Name))));
+        }
+
+        public override PipelinePosition Position => PipelinePosition.PostTransform;
+    }
+}
