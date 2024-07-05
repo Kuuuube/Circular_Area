e90802dfa99b4169bdf353194a9d907682cdadbe
kuuuube <hexagonisalie@gmail.com>
2024-07-04 20:06:43 -0400

    Fix bad args for pow

--- Circular_Area/Circular_Lamé-based_Mapping_Inverse.cs
+++ Circular_Area/Circular_Lamé-based_Mapping_Inverse.cs
@@ -23,8 +23,8 @@ namespace Circular_Area
             double absy = Math.Abs(y);
 
             var circle = new Vector2(
-                (float)((x / Math.Sqrt(x2 + y2)) * Math.Pow((Math.Pow(absx * (2 / ((1 - absx) * (1 - absy)))) + Math.Pow(absy, (2 / ((1 - absx) * (1 - absy))))), (0.5 * (1 - absx) * (1 - absy)))),
-                (float)((y / Math.Sqrt(x2 + y2)) * Math.Pow((Math.Pow(absx * (2 / ((1 - absx) * (1 - absy)))) + Math.Pow(absy, (2 / ((1 - absx) * (1 - absy))))), (0.5 * (1 - absx) * (1 - absy))))
+                (float)((x / Math.Sqrt(x2 + y2)) * Math.Pow((Math.Pow(absx, (2 / ((1 - absx) * (1 - absy)))) + Math.Pow(absy, (2 / ((1 - absx) * (1 - absy))))), (0.5 * (1 - absx) * (1 - absy)))),
+                (float)((y / Math.Sqrt(x2 + y2)) * Math.Pow((Math.Pow(absx, (2 / ((1 - absx) * (1 - absy)))) + Math.Pow(absy, (2 / ((1 - absx) * (1 - absy))))), (0.5 * (1 - absx) * (1 - absy))))
             );
 
             return No_NaN(circle, input);
